df8f4fedfe7b8aa49b7b52660697b29bdbbb2f13
Orestis <orestis.malaspinas@pm.me>
2025-04-23 13:22:07 +0200

    started working on direction parser tests

--- src/parsing/direction_parser.rs
+++ src/parsing/direction_parser.rs
@@ -1,6 +1,15 @@
 // 1 file(s).
 // File(s) read by the parser:
 // RICHTUNG
+/**
+ * This file contains direction informations: namely the Direction ID (that is also contained in
+ * the FPLAN file) and the direction Text which gives the last stop of the traject. From
+ * [https://opentransportdata.swiss/en/cookbook/hafas-rohdaten-format-hrdf/#Technical_description_What_is_in_the_HRDF_files_contents](HRDF the docs) we have:
+ *
+ * `R000011 Esslingen    % Richtung 11 nach Esslingen`
+ *
+ * that the direction 11 (R is for Richtung) travels to Esslingen
+ */
 use std::error::Error;
 
 use rustc_hash::FxHashMap;
@@ -13,16 +22,19 @@ use crate::{
 
 type DirectionAndTypeConverter = (ResourceStorage<Direction>, FxHashMap<String, i32>);
 
-pub fn parse(path: &str) -> Result<DirectionAndTypeConverter, Box<dyn Error>> {
-    log::info!("Parsing RICHTUNG...");
-    #[rustfmt::skip]
-    let row_parser = RowParser::new(vec![
+fn direction_row_parser() -> RowParser {
+    RowParser::new(vec![
         // This row is used to create a Direction instance.
         RowDefinition::from(vec![
             ColumnDefinition::new(1, 7, ExpectedType::String),
             ColumnDefinition::new(9, -1, ExpectedType::String),
         ]),
-    ]);
+    ])
+}
+
+pub fn parse(path: &str) -> Result<DirectionAndTypeConverter, Box<dyn Error>> {
+    log::info!("Parsing RICHTUNG...");
+    let row_parser = direction_row_parser();
     let parser = FileParser::new(&format!("{path}/RICHTUNG"), row_parser)?;
 
     let mut pk_type_converter = FxHashMap::default();
@@ -61,4 +73,27 @@ fn remove_first_char(value: &str) -> &str {
     let mut chars = value.chars();
     chars.next();
     chars.as_str()
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+    use crate::parsing::tests::get_json_values;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn parser_row_d_v207() {
+        let rows = vec!["R000008 Winterthur".to_string()];
+        let parser = FileParser {
+            row_parser: direction_row_parser(),
+            rows,
+        };
+        let mut parser_iterator = parser.parse();
+        // let (id, _, mut parsed_values) = parser_iterator.next().unwrap().unwrap();
+        // let legacy_id: String = parsed_values.remove(0).into();
+        // assert_eq!("VR", &legacy_id);
+        // let description: String = parsed_values.remove(0).into();
+        // assert_eq!("VELOS: Reservation obligatory", &description);
+    }
 }
