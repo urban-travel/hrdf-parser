e42f6c889273b6c89daa70005ab07ef55bbc31c2
Orestis <orestis.malaspinas@pm.me>
2025-09-29 09:42:25 +0200

    working on eckdaten

--- src/parsing/timetable_metadata_parser.rs
+++ src/parsing/timetable_metadata_parser.rs
@@ -4,18 +4,116 @@
 use std::error::Error;
 
 use chrono::NaiveDate;
+use nom::{
+    Parser,
+    branch::alt,
+    bytes::{
+        complete::{take_till1, take_until},
+        tag,
+    },
+    character::complete::{alphanumeric1, digit1, i32, space1, u32},
+    combinator::{map, map_res},
+    multi::{many0, separated_list1},
+    sequence::preceded,
+};
+use rustc_hash::FxHashMap;
 
 use crate::{
     models::{Model, TimetableMetadataEntry},
     parsing::{
         AdvancedRowMatcher, ColumnDefinition, ExpectedType, FastRowMatcher, FileParser,
-        ParsedValue, RowDefinition, RowParser,
+        ParsedValue, RowDefinition, RowParser, helpers::read_lines,
     },
     storage::ResourceStorage,
     utils::AutoIncrement,
 };
 
+enum InfoLines {
+    Date(NaiveDate),
+    MetaData([String; 4]),
+}
+
+fn date_combinator<'a>()
+-> impl Parser<&'a str, Output = InfoLines, Error = nom::error::Error<&'a str>> {
+    map(
+        map_res(
+            (u32, preceded(tag("."), u32), preceded(tag("."), i32)),
+            |(day, month, year)| {
+                NaiveDate::from_ymd_opt(year, month, day)
+                    .ok_or("Unable to parse date {day}, {month}, {year}")
+            },
+        ),
+        InfoLines::Date,
+    )
+}
+
+fn info_combinator<'a>()
+-> impl Parser<&'a str, Output = InfoLines, Error = nom::error::Error<&'a str>> {
+    map(
+        (
+            map(take_until("$"), String::from),
+            separated_list1(tag("$"), map(alphanumeric1, String::from)),
+        ),
+        |(name, other_data)| {
+            InfoLines::MetaData([
+                name,
+                other_data[0].to_owned(),
+                other_data[1].to_owned(),
+                other_data[2].to_owned(),
+            ])
+        },
+    )
+}
+
 pub fn parse(path: &str) -> Result<ResourceStorage<TimetableMetadataEntry>, Box<dyn Error>> {
+    log::info!("Parsing ECKDATEN...");
+    let auto_increment = AutoIncrement::new();
+    let keys = [
+        "start_date",
+        "end_date",
+        "name",
+        "created_at",
+        "version",
+        "provider",
+    ];
+    let mut index = 0;
+    let mut data = FxHashMap::default();
+    read_lines(&format!("{path}/ECKDATEN"), 0)?
+        .into_iter()
+        .filter(|line| !line.trim().is_empty())
+        .try_for_each(|line| {
+            let (_, res) = alt((date_combinator(), info_combinator()))
+                .parse(&line)
+                .map_err(|e| format!("Error: {e}, for line: {line}"))?;
+            match res {
+                InfoLines::Date(d) => {
+                    let tt = TimetableMetadataEntry::new(
+                        auto_increment.next(),
+                        keys[index].to_owned(),
+                        d.to_string(),
+                    );
+                    data.insert(tt.id(), tt);
+                    index += 1;
+                }
+                InfoLines::MetaData(mt) => {
+                    for t in mt {
+                        let tt = TimetableMetadataEntry::new(
+                            auto_increment.next(),
+                            keys[index].to_owned(),
+                            t,
+                        );
+                        data.insert(tt.id(), tt);
+                        index += 1;
+                    }
+                }
+            }
+            Ok::<(), Box<dyn Error>>(())
+        })?;
+
+    Ok(ResourceStorage::new(data))
+}
+
+pub fn old_parse(path: &str) -> Result<ResourceStorage<TimetableMetadataEntry>, Box<dyn Error>> {
     log::info!("Parsing ECKDATEN...");
     const ROW_A: i32 = 1;
     const ROW_B: i32 = 2;
