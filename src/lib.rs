ddf61b03011fb056d328491cabf3e422a09bbda9
Orestis <orestis.malaspinas@pm.me>
2026-01-07 16:17:19 +0100

    added added tests to adapt parser for 2020-2021 formats

--- src/lib.rs
+++ src/lib.rs
@@ -39,27 +39,64 @@ mod tests {
 
     #[test(tokio::test)]
     #[ignore]
-    async fn parsing_2024() {
-        let _hrdf = Hrdf::new(
-            Version::V_5_40_41_2_0_6,
-            "https://data.opentransportdata.swiss/en/dataset/timetable-54-2024-hrdf/permalink",
-            true,
-            None,
-        )
-        .await
-        .unwrap();
+    async fn parsing_2020() {
+        let _hrdf = Hrdf::new(Version::V_5_40_41_2_0_4, "https://archive.opentransportdata.swiss/timetable_hrdf/timetable-2020-hrdf-54/OeV_Sammlung_CH_HRDF_5_40_41_2020_20201207_074253.zip", true, None)
+            .await
+            .unwrap();
     }
 
     #[test(tokio::test)]
     #[ignore]
-    async fn parsing_2025() {
-        let _hrdf = Hrdf::new(
-            Version::V_5_40_41_2_0_7,
-            "https://data.opentransportdata.swiss/en/dataset/timetable-54-2025-hrdf/permalink",
-            true,
-            None,
-        )
-        .await
-        .unwrap();
+    async fn parsing_2021() {
+        let _hrdf = Hrdf::new(Version::V_5_40_41_2_0_5, "https://archive.opentransportdata.swiss/timetable_hrdf/timetable-2021-hrdf-54/OeV_Sammlung_CH_HRDF_5_40_41_2021_20211204_201926.zip", true, None)
+            .await
+            .unwrap();
     }
+
+    // #[test(tokio::test)]
+    // #[ignore]
+    // async fn parsing_2022() {
+    //     let _hrdf = Hrdf::new(Version::V_5_40_41_2_0_6, "https://archive.opentransportdata.swiss/timetable_hrdf/timetable-2022-hrdf-54/OeV_Sammlung_CH_HRDF_5_40_41_2022_20221207_205110.zip", true, None)
+    //         .await
+    //         .unwrap();
+    // }
+
+    // #[test(tokio::test)]
+    // #[ignore]
+    // async fn parsing_2023() {
+    //     let _hrdf = Hrdf::new(
+    //         Version::V_5_40_41_2_0_6,
+    //         "https://archive.opentransportdata.swiss/timetable_hrdf/timetable-2023-hrdf-54/OeV_Sammlung_CH_HRDF_5_40_41_2023_20231206_204217.zip",
+    //         true,
+    //         None,
+    //     )
+    //     .await
+    //     .unwrap();
+    // }
+    //
+    // #[test(tokio::test)]
+    // #[ignore]
+    // async fn parsing_2024() {
+    //     let _hrdf = Hrdf::new(
+    //         Version::V_5_40_41_2_0_6,
+    //         "https://archive.opentransportdata.swiss/timetable_hrdf/timetable-2024-hrdf-54/OeV_Sammlung_CH_HRDF_5_40_41_2024_20241213_205621.zip",
+    //         true,
+    //         None,
+    //     )
+    //     .await
+    //     .unwrap();
+    // }
+    //
+    // #[test(tokio::test)]
+    // #[ignore]
+    // async fn parsing_2025() {
+    //     let _hrdf = Hrdf::new(
+    //         Version::V_5_40_41_2_0_7,
+    //         "https://archive.opentransportdata.swiss/timetable_hrdf/timetable-2025-hrdf-54/OeV_Sammlung_CH_HRDF_5_40_41_2025_20251209_205941.zip",
+    //         true,
+    //         None,
+    //     )
+    //     .await
+    //     .unwrap();
+    // }
 }
